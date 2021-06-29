74b5eb663ad92165a46e1d72ea8dc7eaf55f026e
Violina Eladii <violina.eladii@internal.ebs.md>
2021-06-29 14:19:51 +0300

    First commit

--- src/task3/MyAnagrams.java
+++ src/task3/MyAnagrams.java
@@ -6,22 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-public class Main {
-
-
-    private static void GetFileInfo() {
-        File myObj = new File("filename.txt");
-        if (myObj.exists()) {
-            System.out.println("File name: " + myObj.getName());
-            System.out.println("Absolute path: " + myObj.getAbsolutePath());
-            System.out.println("Writeable: " + myObj.canWrite());
-            System.out.println("Readable " + myObj.canRead());
-            System.out.println("File size in bytes " + myObj.length());
-        } else {
-            System.out.println("The file does not exist.");
-        }
-
-    }
+public class MyAnagrams {
 
     private static void CreateFile() {
         File myObj = new File("filename.txt");
@@ -58,32 +43,58 @@ public class Main {
 
     }
 
-    private static void ReadFile() {
+    private static String[] ReadFile() {
+        String[] words = null;
         try {
+            String data = "";
             File myObj = new File("filename.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                data += myReader.nextLine() + " ";
             }
+            words = data.split(" ");
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return words;
     }
 
 
+    private static void printAnagrams(String arr[]) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            String word = arr[i];
+            char[] letters = word.toCharArray();
+
+            Arrays.sort(letters);
+            String newWord = new String(letters);
+
+            if (map.containsKey(newWord)) {
+                map.get(newWord).add(word);
+            } else {
+                List<String> words = new ArrayList<>();
+                words.add(word);
+                map.put(newWord, words);
+            }
+        }
+        for (String s : map.keySet()) {
+            List<String> values = map.get(s);
+            if (values.size() > 1) {
+                System.out.print(values);
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        File myObj = new File("filename.txt");
-
         CreateFile();
         WriteToFile();
-        GetFileInfo();
-        ReadFile();
 
-
+        String[] words = ReadFile();
+        printAnagrams(words);
     }
-
 }
